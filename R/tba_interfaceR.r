808ca6f80b797953bee2a9dde20039a169025cd0
Gabriel Krotkov <gabriel.krotkov@thinkesi.com>
2023-04-03 15:37:33 -0500

    80 character limit edit

--- R/tba_interfaceR.r
+++ R/tba_interfaceR.r
@@ -275,7 +275,7 @@ event_matches <- function(event_key, alliances = TRUE, breakdown = TRUE,
 
     if (keys) return(unlist(data))
 
-    if (is.null(data) | length(data) == 0){ # case where no matches are yet posted
+    if (is.null(data) | length(data) == 0){ # case where no matches are posted
         warning("event_matches() call with no posted schedule; returning NA")
         return(NA)
     }
