cb811521f2d2777500625ac7e6415d6062153eb8
gkrotkov <gabriel.gk.mail@gmail.com>
2025-02-28 05:47:06 -0500

    reworking dependencies

--- R/dependencies.R
+++ R/dependencies.R
@@ -13,15 +13,43 @@
 #' @importFrom httr GET
 #' @importFrom here here
 #' @importFrom withr with_dir
+#' @importFrom tibble tibble
 
-packages <- c("devtools", "jsonlite", "httr", "sys", "stringr", "rvest",
-              "MASS", "tidyverse", "data.table", "roxygen2",
-              "withr", "here", "pracma", "renv")
+#' Install Dependencies
+#' @details
+#' Some packages we don't actually depend on (like devtools) we still want
+#' the user to install.
+#'
+install_dependencies <- function(){
+    packages <- c(
+        "devtools", "jsonlite", "httr", "sys", "stringr", "rvest", "MASS",
+        "tidyverse", "data.table", "roxygen2", "withr", "here", "pracma",
+        "renv","tibble"
+    )
+    installed_packages <- packages %in% rownames(installed.packages())
 
-# Install missing packages
-installed_packages <- packages %in% rownames(installed.packages())
-if (any(installed_packages == FALSE)) {
-    install.packages(packages[!installed_packages], dependencies = TRUE)
+    if(any(!installed_packages)){
+        install.packages(packges[!installed_packages], dependencies = TRUE)
+    }
 }
 
-invisible(lapply(packages, library, character.only = TRUE))
+#' Initialize scoutR
+#'
+#' Function to start scoutR by writing the auth key for scoutR as well as
+#' creating the documentation files for scoutR
+#' @export
+initialize_scoutR <- function(auth_key = NULL){
+    if (is.null(auth_key)){
+        warning("No auth key supplied. We assume data/tba_auth_key.txt exists.")
+    }
+    else{
+        with_dir(here(),{
+            # first, if `data` does not exist, create it
+            if (!dir.exists("data")) dir.create("data", recursive = TRUE)
+            # then create the auth key text file
+            write_file(auth_key, file = "data/tba_auth_key.txt")
+        })
+    }
+    install_dependencies()
+}
+
