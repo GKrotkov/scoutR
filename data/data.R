28a4e8f2a17e9dd13e8fd2b2ddf237cb96203b88
gkrotkov <gabriel.gk.mail@gmail.com>
2025-03-01 00:24:24 -0500

    exporting some data from the package

---
+++ data/data.R
@@ -0,0 +1,6 @@
+
+#' Optimized OPR Weights
+#'
+#' @format A vector of stepwise-optimized weights to improve OPR
+#' @source "Clutch Matches Are In The Middle", by Girls of Steel Data Science
+"optimized_opr_weighting"
