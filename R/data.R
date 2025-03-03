fcf6051ad849bd4e0b68b791fccf6d6d4ba513c8
Gabriel Krotkov <gabriel.krotkov@fda.hhs.gov>
2025-03-03 15:39:41 -0500

    data documentation file

---
+++ R/data.R
@@ -0,0 +1,24 @@
+# data documentation file
+
+#' District Qualification Data (until 09/2024)
+#'
+#' A list of qualification data from every district played between 2009 and
+#' 2024. Every item in the list is a dataframe, and each dataframe has at least
+#' the columns "red1", "red2", "red3", "blue1", "blue2", "blue3", "red_score",
+#' and "blue_score"
+#' @source thebluealliance.com
+"district_quals_09_24"
+
+#' Stepwise-Optimized OPR Weights
+#'
+#' Linear Regression weights optimized stepwise as outlined by Girls of Steel
+#' in "Clutch Matches are in the Middle"
+#'
+#' @source "Clutch Matches are in the Middle, by Girls of Steel"
+"optimized_opr_weighting"
+
+#' All Team Awards (2024)
+#'
+#' List of all team award histories
+#' @source thebluealliance.com
+"awards/all_team_awards"
