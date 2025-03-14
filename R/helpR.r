289f1b6e415c010a443980b0f51a15898b9aad2f
gkrotkov <gabriel.gk.mail@gmail.com>
2025-03-14 09:56:21 -0400

    adding combine_tbls

--- R/helpR.r
+++ R/helpR.r
@@ -91,6 +91,28 @@ rep_each_len <- function(v, len_out){
     return(rep(v, times = baseline + adj))
 }
 
+#' Combine Tables
+#'
+#' Combines two tables, numerical vectors of integer counts. Assumes that
+#' order() is a meaningful call on the names of the tables.
+#' @param tbl1 First table to be combined
+#' @param tbl2 Second table to be combined
+#' @export
+combine_tbls <- function(tbl1, tbl2){
+    unq <- unique(c(names(tbl1), names(tbl2)))
+    # using 0 bc we will combine with addition
+    pads <- rep(0, length(unq) - length(tbl1))
+    names(pads) <- setdiff(unq, names(tbl1))
+    tbl1 <- c(tbl1, pads)
+    tbl1 <- tbl1[order(names(tbl1))]
+    # using 0 bc we will combine with addition
+    pads <- rep(0, length(unq) - length(tbl2))
+    names(pads) <- setdiff(unq, names(tbl2))
+    tbl2 <- c(tbl2, pads)
+    tbl2 <- tbl2[order(names(tbl2))]
+    return(tbl1 + tbl2)
+}
+
 #' Normalize Weights
 #'
 #' Takes a vector of real, non-negative weights and converts them to an integer
