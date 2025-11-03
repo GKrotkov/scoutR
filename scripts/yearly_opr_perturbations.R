2c5a88c31c85f61abda735506e2b4d28d334d84b
Gabriel Krotkov <gabriel.krotkov@fda.hhs.gov>
2025-11-03 17:39:26 -0500

    perturbs run for 2018 to 2025

--- scripts/yearly_opr_perturbations.R
+++ scripts/yearly_opr_perturbations.R
@@ -7,12 +7,14 @@ devtools::load_all()
 # skip COVID years
 years <- setdiff(2012:2025, 2020:2021)
 
-years <- 2022:2025
+years <- c(2018, 2019, 2022, 2023, 2024, 2025)
 
 perturbs <- data.frame()
 
 for (i in seq_along(years)){
     perturbs <- rbind(perturbs, year_opr_perturbs(years[i]))
 }
+
+perturbs <- perturbs[complete.cases(perturbs), ]
 
 save(perturbs, file = "data/perturbs.rda")
